f5eb29fadfa284c93496c98bcb14837996fe22b4
Alen J <alenj@Alenj.local>
2023-11-23 15:48:20 +0700

    navbar page responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header section__padding'>
+      <div className='header-content'>
+        <h1 >Supercharge you aquaculture business</h1>
+        <p>Elevate your farm's productivity and boost ROI with our integrated farm management platform.</p>
+        <div className='header-content_buttons'>
+          <button type='button'>Request demo</button>
+          <button type='button'>Know more</button>
+        </div>
+      </div>
+      <div className='header-media'>
+        {/* video to be added */}
+      </div>
+    </div>
   )
 }
 
