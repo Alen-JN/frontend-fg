3481b27ea505e556adf3e2477cc1f10a35b62db8
Sreela K <sreelak@ITs-MacBook-Pro-3.local>
2023-11-27 01:41:28 +0530

    modal

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './header.css'
-
+import Modal from '../modal/modal'
 function Header() {
 
   const videopath = require('../../media/random1.mp4')
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  // const [showModal, setShowModal] = useState(false);
+
+  // const handleRequestDemo = () => {
+  //   setShowModal(true)
+  // }
+
+  // const handleCloseModal = () => {
+  //   setShowModal(false)
+  // }
   return (
     <div className='header section__padding'>
       <div className='header-content'>
         <h1> <span>Supercharge</span> you aquaculture business</h1>
         <p>Elevate your farm's productivity and boost ROI with our integrated farm management platform.</p>
         <div className='header-content_btn'>
-          <button type='button' className='header-content_btn-1'>Request demo</button>
+        <button type='button' className='header-content_btn-1' onClick={openModal}>
+            Request Demo
+          </button>
+          {isOpen && <Modal isOpen={isOpen} closeModal={closeModal} />}
+
+          {/* {showModal && <Modal showModal={showModal} handleCloseModal={handleCloseModal} />} */}
           <button type='button' className='header-content_btn-2'>Know more</button>
         </div>
       </div>
--- src/containers/modal/modal.jsx
+++ src/containers/modal/modal.jsx
@@ -1,24 +1,169 @@
-import React from 'react'
 
-function modal() {
+
+import modal from '../../media/modal1.svg'
+import React, { useState } from 'react';
+import './modal.css'; // You can define your styles in a separate CSS file
+const Modal = ({isOpen,closeModal}) => {
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setIsOpen(true);
+  // };
+
+  // const closeModal = () => {
+  //   setIsOpen(false);
+  // };
+
+  const handleSubmit = () => {
+    // Add your form submission logic here
+    // For example, you can collect form data and perform any necessary actions
+    // Then close the modal
+    closeModal();
+  };
+
   return (
-    // <div className='modal-bg>
-    <div className='modal-bg'>
-    <div className='Modal'>
-        <div className='image'>
-        </div>
-        <div className='Form'>
-            <div className='modal-header'>
-                <h6>Let’s connect...</h6>
-            </div>
-            <div className='Formlist'>
+    <div>
+      {/* <button onClick={openModal}>Open Modal</button> */}
+      {isOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal" onClick={(e) => e.stopPropagation()}>
+           
+              <button className="close-button" onClick={closeModal}>
+                &times;
+              </button>
             
-            </div>
-        </div>
-    </div>
-    </div>
-   
-  )
-}
+            <div className="modal-content">
+              <div className="left-section">
+                {/* Add your image here */}
+                <img
+                  src={modal}
+                  alt="Sample Image"
+                />
+              </div>
+              <div className="right-section">
+                <h2>Let's Connect...</h2>
+                <form>
+                <label
+htmlFor='name'>Name</label>
 
-export default modal
+          
+<input
+type='text'
+id='companyname'
+name='companyname'
+required />     
+<label
+htmlFor='name'>Company Name</label>
+
+          
+<input
+type='text'
+id='designation'
+name='name'
+required />
+<label
+htmlFor='name'>Designation</label>
+
+          
+<input
+type='text'
+id='name'
+name='name'
+required /> 
+<label
+htmlFor='email'>Email:</label>
+<input
+type='email'
+id='email'
+name='email'
+required />   
+      <label htmlFor="mobile">Mobile No</label>
+<input type="tel" id="mobile" name="mobile" required />
+<label
+htmlFor='message'>Message</label>
+<textarea
+id='message'
+name='message' rows='5' required></textarea>
+
+                  <div className="modal-buttons">
+                    
+                    <button type="button" onClick={handleSubmit}>
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+     
+    </div>
+  );
+};
+
+export default Modal;
+
+// import React from 'react'
+
+// const Modal = ({ showModal, handleCloseModal }) => {
+//   return (
+//     <div className='modal'>
+//       <div className='modal-content'>
+//         <h2>Request a Demo</h2>
+
+        
+// <form>    
+// <label
+// htmlFor='name'>Name</label>
+
+          
+// <input
+// type='text'
+// id='companyname'
+// name='companyname'
+// required />     
+// <label
+// htmlFor='name'>Company Name</label>
+
+          
+// <input
+// type='text'
+// id='designation'
+// name='name'
+// required />
+// <label
+// htmlFor='name'>Designation</label>
+
+          
+// <input
+// type='text'
+// id='name'
+// name='name'
+// required /> 
+// <label
+// htmlFor='email'>Email:</label>
+// <input
+// type='email'
+// id='email'
+// name='email'
+// required />   
+//       <label htmlFor="mobile">Mobile No</label>
+// <input type="tel" id="mobile" name="mobile" required />
+// <label
+// htmlFor='message'>Message</label>
+// <textarea
+// id='message'
+// name='message' rows='5' required></textarea>
+//           <button type='submit'>Submit</button>
+//         </form>
+//         <button className='modal-close-btn' onClick={handleCloseModal}>
+//           X
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Modal
